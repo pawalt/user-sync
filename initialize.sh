a720345bb2b13fd6ad99c3ff40b5302296c73823
Peyton Walters <pwpon500@gmail.com>
2017-03-07 22:03:00 -0500

    Adding plugin insatlling

--- initialize.sh
+++ initialize.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 echo "source ~/user-sync/vimrc" > ~/.vimrc
-
 ln -s ~/user-sync/vim ~/.vim
 
 echo ". ~/user-sync/bash_aliases" > ~/.bash_aliases
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
