5821f5b2c6854011b00ac203751566fd39f89de2
Peyton Walters <pwpon500@gmail.com>
2017-03-08 18:00:17 -0500

    Added gnome terminal theme

--- initialize.sh
+++ initialize.sh
@@ -4,6 +4,7 @@ echo "source ~/user-sync/vimrc" > ~/.vimrc
 ln -s ~/user-sync/vim ~/.vim
 
 echo ". ~/user-sync/bash_aliases" > ~/.bash_aliases
+wget -O xt  http://git.io/v6JYg && chmod +x xt && ./xt && rm xt
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
