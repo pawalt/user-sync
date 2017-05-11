71d5bd832385ef1d7a22a53f8c466cb45a8e2c61
Peyton Walters <peyton@peyton-suse.home>
2017-05-11 18:31:30 -0400

    Added bashrc

---
+++ bashrc
@@ -0,0 +1,3 @@
+PS1='\n\033[33;1m\u@\h\033[0;32m \t\033[0m \n > \w \$ '
+
+source ~/.bash_aliases
--- initialize.sh
+++ initialize.sh
@@ -4,6 +4,8 @@ echo "source ~/user-sync/vimrc" > ~/.vimrc
 ln -s ~/user-sync/vim ~/.vim
 
 echo ". ~/user-sync/bash_aliases" > ~/.bash_aliases
+echo ". ~/user-sync/bashrc" > ~/.bashrc
+
 wget -O xt  http://git.io/v6JYg && chmod +x xt && ./xt && rm xt
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
