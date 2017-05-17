d634306bec00aff7ca914a0c7ff0bfc35cb939bf
Peyton Walters <pwpon500@gmail.com>
2017-05-16 20:54:12 -0400

    Added fish to initialize

--- initialize.sh
+++ initialize.sh
@@ -6,6 +6,8 @@ ln -s ~/user-sync/vim ~/.vim
 echo ". ~/user-sync/bash_aliases" > ~/.bash_aliases
 echo ". ~/user-sync/bashrc" > ~/.bashrc
 
+ln -s ~/user-sync/functions ~/.config/fish/functions
+
 wget -O xt  http://git.io/v6JYg && chmod +x xt && ./xt && rm xt
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
