4cfe52d2bf0bc5d60647573b803aa5b4c186d91b
Peyton Walters <pwpon500@gmail.com>
2017-05-21 13:20:08 -0400

    Fixed fish stuff

---
+++ config.fish
@@ -0,0 +1,44 @@
+function cvn
+	cd /opt/; l
+end
+
+function fish_prompt
+	set_color FB0 --bold
+	echo
+	echo "$USER"@(prompt_hostname)(set_color normal)(set_color 9AA) (date +%T)
+	set_color normal
+	echo '' (prompt_pwd) '> '
+end
+
+function l
+	ls -CF $argv
+end
+
+function ll
+	ls -alh $argv
+end
+
+function p	
+	ping $argv
+end
+
+function pg
+	ping google.com
+end
+
+function rg
+	grep -i -r $argv
+end
+
+function ssr
+	ssh -l root $argv
+end
+
+function svq
+	svn update
+	svn status
+end
+
+function vime
+	vim "+set mouse=a" $argv
+end
