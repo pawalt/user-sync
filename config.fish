5566db10ef1a2c39cb2ec06e2339e01be3e464b7
Peyton Walters <pwpon500@gmail.com>
2017-06-01 17:47:39 -0400

    lit af

--- config.fish
+++ config.fish
@@ -5,7 +5,7 @@ end
 function fish_prompt
 	set_color FB0 --bold
 	echo
-	echo "$USER"@(prompt_hostname)(set_color normal)(set_color 9AA) (date +%T)
+	echo "$USER"@(hostname)(set_color normal)(set_color 9AA) (date +%T)
 	set_color normal
 	echo '' (prompt_pwd) '> '
 end
