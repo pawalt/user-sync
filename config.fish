7e95fe61d2be5e4218d9e8d668638bbfeaff3873
Peyton Walters <pwpon500@gmail.com>
2017-07-30 10:05:50 -0400

    Added forced fish colors

--- config.fish
+++ config.fish
@@ -48,9 +48,31 @@ function note
 end
 
 function hw
-	task project.contains:school urgency.above:0 next
+	task project.contains school urgency.above:0 next
 end
 
 function work
-	task project.contains:work urgency.above:0 next
+	task project.contains work urgency.above:0 next
 end
+
+set EDITOR vim
+set fish_color_cwd green
+set fish_color_cwd_root red
+set fish_color_command FF9400
+set fish_color_comment A63100
+set fish_color_autosuggestion FFC473
+set fish_color_end FF4C00
+set fish_color_escape cyan
+set fish_color_history_current cyan
+set fish_color_host normal
+set fish_color_match cyan
+set fish_color_normal normal
+set fish_color_operator cyan
+set fish_color_param FFC000
+set fish_color_quote BF9C30
+set fish_color_redirection BF5B30
+set fish_color_search_match \x2d\x2dbackground\x3dpurple
+set fish_color_selection \x2d\x2dbackground\x3dpurple
+set fish_color_user green
+set fish_color_valid_path \x2d\x2dunderline
+set fish_color_error ff0000
